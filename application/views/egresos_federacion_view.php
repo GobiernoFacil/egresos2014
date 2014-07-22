543a8f945d112824742e2517b6fa18922a911702
hugovom <hugo.om@gmail.com>
2014-07-21 20:10:47 -0500

    add html markup

--- application/views/egresos_federacion_view.php
+++ application/views/egresos_federacion_view.php
@@ -1,23 +1,44 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-  <meta charset="utf8">
-  <title>Presupuesto de egresos de la federación 2014</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Presupuesto de egresos de la federación 2014</title>
+	<meta name="description" content="Presupuesto de egresos de la federación 2014">
+	<meta name="viewport" content="width=device-width">
+	
+	<link rel="stylesheet" href="/css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="/css/styles.css" />
 </head>
 <body>
-  <div id="main" class="egresos2014">
-    <h1>Presupuesto de egresos de la federación 2014</h1>
+<header>
+	<div class="container">
+		<div class="col-md-8">
+			<h1>México | Presupuesto de egresos de la federación 2014</h1>
+		</div>
+		<div class="col-md-4">
+			<h2>$4,467,225,800,000 <small>MXN</small></h2>
+		</div>
+		<!-- en el futuro-
+		 <form id="interface">
+		 <p><input type="text" name="query" id="query"></p>
+		 </form>-->
+	</div>
+</header><!-- header ends-->
+<div class="container">
+	<div id="main" class="egresos2014 row">
 
     <!-- aquí van los controles para navegar en el presupuesto
          de egresos de la federación -->
 
-    <form id="interface">
-      <p><input type="text" name="query" id="query"></p>
-    </form>
-
     <ul id="ramos-list"></ul>
 
-  </div>
+   </div>
+
+</div>
 
   <!-- carga el require.js 
        todo el código está dentro de js/apps/egresos2014  -->
